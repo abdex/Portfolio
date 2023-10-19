08be280ba62faeae29fa1757a7fb4b00026128ba
Abby A <abidextrous@gmail.com>
2023-10-19 10:01:53 +0100

    delayed adding lots of THINGS because i broke git - connected json with javascri pt, positioning improved all pages, uploaded png files to gitHub

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("test")
+fetch('/portfolio.json').then(function (response) {
+    return response.json()
+}).then(function (data) {
+
+const displayTarget = document.querySelector('.target')
+
+let output = '';
+
+data.projects.forEach(function (item) {
+    
+    output += '<div class="projects">'
+    output += '<h2>' + item.name + '</h2>'
+    output += '<p>' + item.date + '</p>'
+    output += '<p>' + item.details + '</p>'
+    output += "<a href='#'><p>" + item.location + "</p></a>"
+    output += '<div class="project_images"><img src="' + item.image + '" /></div>'
+    output += '</div>'
+})
+
+displayTarget.innerHTML = output
+})
+
+
